4003a8f7bbf9e07d3e49785e199c75f5af614eb9
tlchou <amo2468man21@gmail.com>
2018-10-13 17:50:08 +0800

    change rm_bkgnd method

---
+++ color_rm_bkgnd.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import os
+
+
+def rm_background(DATA_DIR):
+    for filename in os.listdir(DATA_DIR):
+        img  = cv2.imread(filename)
+        #img=cv2.resize(img,None,fx=0.5,fy=0.5)
+        rows,cols,channels = img.shape
+        #转换hsv
+        hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+        lower_blue=np.array([0,0,0])   #bad 0 0 0
+        upper_blue=np.array([180,255,50]) #bad 180 255 40
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        #cv2.imshow('Mask', mask)
+
+        #腐蚀膨胀
+        erode=cv2.erode(mask,None,iterations=1)
+        #cv2.imshow('erode',erode)
+        dilate=cv2.dilate(erode,None,iterations=1)
+        #cv2.imshow('dilate',dilate)
+
+        #遍历替换
+        for i in range(rows):
+            for j in range(cols):
+                if dilate[i,j]==255:
+                    img[i,j]=(0,0,0)#此处替换颜色，为BGR通道
+        #cv2.imshow('res',img)
+        cv2.imwrite('./result/rm_bkgnd/' + os.path.basename(filename), img)
+    #    cv2.waitKey(10000)
+    #    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os 
+import os
 import cv2
 from hotelscombine import cut_and_label
-from rm_background4 import rm_background
+# from rm_background4 import rm_background
+from color_rm_bkgnd import rm_background
 from just_rotate import just_rotate
 location = "./augmentation"
 
@@ -10,7 +11,7 @@ type = input('If data in '+str(location)+' are good, please type 1; if bad, plea
 while(type != '1' and type != '2'):
     print('Incorrect input, please type again!')
     type = input('If data in '+str(location)+' are good, please type 1; if bad, please type 2 : ')
-    
+
 if type == '1':
     label_type = True
 else:
@@ -18,10 +19,11 @@ else:
 
 # cut_and_label(String DATA_DIR, Boolean label_or_not, String STORE_DIR, Boolean GOOD, Boolean flip)
 cut_and_label(location, False, "./result/cut", label_type, False)
+
 # rm_background(String DATA_DIR)
 # Automatically remove background, rotate photos, and save in './result/rotate'
 
-#rm_background("./result/cut")
-just_rotate("./result/cut")
+rm_background("./result/cut")
+just_rotate("./result/rm_bkgnd")
 
 cut_and_label("./result/rotate", True, "./result/cut_after_rotate", label_type, True)
